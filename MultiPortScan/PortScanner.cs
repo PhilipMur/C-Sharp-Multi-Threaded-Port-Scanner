3a34e43fe09c6e2d1f11f7c242b67070467b8c9e
Philip Murray <philipmurray2009@gmail.com>
2018-02-09 20:09:50 +0000

    Added Port scan features added
    Features added similar to nmap and zenmap like banner grabbing and webpage title grabbing

--- MultiPortScan/PortScanner.cs
+++ MultiPortScan/PortScanner.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.IO;
+using System.Net;
 using System.Net.Sockets;
+using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading;
 
 namespace MultiPortScan
@@ -68,6 +72,37 @@ namespace MultiPortScan
                 Console.ForegroundColor = ConsoleColor.Green;
                 Console.WriteLine();
                 Console.WriteLine("TCP Port {0} is open ", port);
+                try
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    //grabs the banner / header info etc..
+                    Console.WriteLine(BannerGrab(host, port, tcpTimeout)); 
+                   
+                   
+                }
+                catch (Exception ex)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Could not retrieve the Banner ::Original Error = " + ex.Message);
+                    Console.ResetColor();
+                }
+                Console.ForegroundColor = ConsoleColor.Green;
+                string webpageTitle = GetPageTitle("http://" + host + ":" + port.ToString());
+
+                if(!string.IsNullOrWhiteSpace(webpageTitle))
+                {
+                    //this gets the html title of the webpage
+                    Console.WriteLine("Webpage Title = " + webpageTitle + "Found @ :: " + "http://" + host + ":" + port.ToString()); 
+                  
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkMagenta;
+                    Console.WriteLine("Maybe A Login popup or a Service Login Found @ :: " + host + ":" + port.ToString());
+                    Console.ResetColor();
+                }
+               
+
                 Console.ResetColor();
 
             }
@@ -105,6 +140,33 @@ namespace MultiPortScan
             }
             return newClient;
         }
+
+        //method for Grabbing a webpage banner / header information
+        public string BannerGrab(string hostName, int port, int timeout)
+        {
+            var newClient = new TcpClient(hostName ,port);
+
+           
+            newClient.SendTimeout = timeout;
+            newClient.ReceiveTimeout = timeout;
+            NetworkStream ns = newClient.GetStream();
+            StreamWriter sw = new StreamWriter(ns);
+
+            //sw.Write("GET / HTTP/1.1\r\n\r\n");
+
+            sw.Write("HEAD / HTTP/1.1\r\n\r\n"
+                + "Connection: Closernrn");
+
+            sw.Flush();
+
+            byte[] bytes = new byte[2048];
+            int bytesRead = ns.Read(bytes, 0, bytes.Length);
+            string response = Encoding.ASCII.GetString(bytes, 0, bytesRead);
+
+            return response;
+        }
+
+
         //async callback for tcp clients
         void AsyncCallback(IAsyncResult asyncResult)
         {
@@ -127,6 +189,29 @@ namespace MultiPortScan
                
             client.Close();
         }
-        
+
+        static string GetPageTitle(string link)
+        {
+            try
+            {
+
+                WebClient x = new WebClient();
+                string sourcedata = x.DownloadString(link);
+                string getValueTitle = Regex.Match(sourcedata, @"\<title\b[^>]*\>\s*(?<Title>[\s\S]*?)\</title\>", RegexOptions.IgnoreCase).Groups["Title"].Value;
+              
+                return getValueTitle;
+              
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Could not connect. Error:" + ex.Message);
+                Console.ResetColor();
+                return "";
+            }
+
+           
+        }
+
     }
 }
